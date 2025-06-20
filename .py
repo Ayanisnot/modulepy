2ad90a1bf0eaa6fe18b38d65408ab8a772506c25
Ayanisnot <alayanhussain0@gmail.com>
2025-06-20 20:24:44 +0600

    Add files via upload

--- .py
+++ .py
@@ -1,11 +1,20 @@
-from datetime import date , datetime
+def hotel_cost(nights):
+    return 140*nights
 
-today = date.today()
-
-now = datetime.now()
-
-print("Todays date is :" , today)
-
-print("\nCurrent Date and time :" , now)
-
-print("\n Data components:", today.year, today.month , today.day  )
+def plane_ride_cost(city):
+    if "charlotte" == city:
+        return 220
+    elif "tampa" == city:
+        return 699
+    elif "pittsburgh" == city:
+        return 547
+    elif "los angeles" == city:
+        return 901
+    
+def rental_car_cost(days):
+    if days>= 7
+       return 40*days - 50
+    elif days>= 3
+       return 40*days - 20
+    else :
+        return 40*days
