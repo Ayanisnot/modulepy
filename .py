bd36d301dde0d5e35717b131edb7abc11b034ab8
Ayanisnot <alayanhussain0@gmail.com>
2025-06-26 21:10:05 +0600

    Add files via upload

--- .py
+++ .py
@@ -1,20 +1,32 @@
-def hotel_cost(nights):
-    return 140*nights
+empty_lists = []
+print(empty_lists)
+print()
+print()
 
-def plane_ride_cost(city):
-    if "charlotte" == city:
-        return 220
-    elif "tampa" == city:
-        return 699
-    elif "pittsburgh" == city:
-        return 547
-    elif "los angeles" == city:
-        return 901
-    
-def rental_car_cost(days):
-    if days>= 7
-       return 40*days - 50
-    elif days>= 3
-       return 40*days - 20
-    else :
-        return 40*days
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+print()
+print()
+
+triples = [1, 2, 3] * 3
+print(triples)
+print()
+print()
+
+
+
+aList = [100, 200, 300, 400]
+aList = aList[:: -1]
+print(aList, "\n")
+print()
+print()
+
+
+print(numbers[3])
+print()
+print()
+
+
+print(numbers[1:3])
+print()
+print()
