74d2047071fcca11a188f3eff2b36fdcf78c3f9a
Ayanisnot <alayanhussain0@gmail.com>
2025-06-26 21:27:08 +0600

    Add files via upload

--- .py
+++ .py
@@ -1,32 +1,18 @@
-empty_lists = []
-print(empty_lists)
-print()
+mylist = [3, 4, 5, 7, 1, 9]
+print("original list : " , mylist)
+
+count = 0
+
+for i in mylist:
+    count = count + i
+
+avg = count/len(mylist)
+
+print("sum :" , count)
+print("average :" , avg)
 print()
 
-numbers = [1, 2, 3, 4, 5]
-print(numbers)
-print()
-print()
+mylist.sort()
+print(mylist)
 
-triples = [1, 2, 3] * 3
-print(triples)
-print()
-print()
-
-
-
-aList = [100, 200, 300, 400]
-aList = aList[:: -1]
-print(aList, "\n")
-print()
-print()
-
-
-print(numbers[3])
-print()
-print()
-
-
-print(numbers[1:3])
-print()
-print()
+print("small element is " , mylist[0])
