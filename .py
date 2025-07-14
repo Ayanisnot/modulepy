8f3c9bf831a5d03c03fd4849eb4fc4138815c278
Ayanisnot <alayanhussain0@gmail.com>
2025-07-14 22:05:59 +0600

    Add files via upload

--- .py
+++ .py
@@ -1,8 +1,12 @@
-import math
+birthday1 = "2010-4-17"
+birthday2 = "2011-9-27"
+birthday3 = "2010-6-10"
+birthday4 = "2010-2-17"
+birthday5 = "2010-9-21"
 
-num = float(input("Enter a number to find its square root :"))
-if num < 0:
-    print("square root is not defined for negative number")
-else:
-    sqrt = math.sqrt(num)
-    print(f"The square root of {num} is {sqrt}")
+
+print("birthday 1:" , birthday1)
+print("birthday 2:" , birthday2)
+print("birthday 3:" , birthday3)
+print("birthday 4:" , birthday4)
+print("birthday 5:" , birthday5)
